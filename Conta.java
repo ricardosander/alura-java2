bb186cc96b774dbaa2b25f11a8acf12f6403ba8e
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 23:42:54 -0300

    Exercício 1, Módulo 2

---
+++ Conta.java
@@ -0,0 +1,22 @@
+class Conta {
+	
+	private double saldo;
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public void deposita(double deposito) {
+		this.saldo += deposito;
+	}
+
+	public void saca(double saque) {
+
+		if (this.saldo < saque) {
+
+			System.out.println("Saldo insuficiente.");
+			return;
+		}
+		this.saldo -= saque;
+	}
+}
