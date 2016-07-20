3fa10e3e663ef2967698b35fe9cf58616e7fbcf3
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-20 00:15:51 -0300

    Módulo 4, Exercícios 4: Implementando e sobreescrevendo métodos.

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -2,6 +2,7 @@ package java2;
 
 public class ContaCorrente extends Conta {
 
+	@Override
 	public void atualiza(double taxa) {
 		this.saldo += this.saldo * taxa * 2;
 	}
--- ContaPoupanca.java
+++ ContaPoupanca.java
@@ -2,10 +2,12 @@ package java2;
 
 public class ContaPoupanca extends Conta {
 
+	@Override
 	public void atualiza(double taxa) {
 		this.saldo += this.saldo * taxa * 3;
 	}
 
+	@Override
 	public void deposita(double deposito) {
 		this.saldo += deposito - 0.1;
 	}
