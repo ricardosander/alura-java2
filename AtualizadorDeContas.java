75d61ed2bd866a0b0c061dd25bbfc353512aa8e0
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-19 23:50:10 -0300

    Exercício 6, Módulo 2

---
+++ AtualizadorDeContas.java
@@ -0,0 +1,23 @@
+class AtualizadorDeContas {
+
+	private double saldoTotal;
+	private double selic;
+
+	public double getSaldoTotal() {
+		return this.saldoTotal;
+	}
+
+	public AtualizadorDeContas(double selic) {
+
+		this.selic = selic;
+		this.saldoTotal = 0.0;
+	}
+
+	public void roda(Conta conta) {
+
+		System.out.println("Saldo anterior: " + conta.getSaldo());
+		conta.atualiza(this.selic);
+		System.out.println("Saldo final: " + conta.getSaldo());
+		this.saldoTotal += conta.getSaldo();
+	}
+}
